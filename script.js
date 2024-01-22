b9bc4e6690467b35ee2b55accb0884851e327ff5
AmareshBale <137062501+AmareshBale@users.noreply.github.com>
2024-01-22 12:25:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function rotateLine() {
+	const line = document.getElementById("line");
+	let currentAngle = parseInt(line.style.transform.replace('rotate(', '').replace('deg)', '')) || 0;
+	currentAngle += 2;
+	line.style.transform=`rotate(${currentAngle}deg)`;
+	
+}
+setInterval(rotateLine,20)
